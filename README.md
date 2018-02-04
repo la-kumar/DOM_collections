be2844a9dd4d607d30ae65adfe2ebb65a6b6b8ea
lalit kumar <lalit.kumar@geminisolutions.in>
2018-02-04 23:07:12 +0530

    working fine

---
+++ README.md
@@ -0,0 +1,1 @@
+# DOM_collections this is small code to get a little example of how to change css property of html element using DOM's collection
---
+++ myScript.js
@@ -0,0 +1,33 @@
+    var rows = document.getElementsByTagName("tr");
+
+
+    function changeColor(rowNum) {
+        var i = 0;
+        //console.log(rowNum);
+        // //console.log(rows[0].cells[0].innerHTML);
+        //console.log(rows[rowNum].cells.length);
+        for (i = 0; i < 6; i++) {
+            var myStyle = rows[rowNum].cells[i].style;
+            if (i % 2 === 0) {
+                if (myStyle.backgroundColor == "red") {
+                    myStyle.backgroundColor = "blue";
+                    myStyle.foregroundColor = "yellow";
+                } else {
+                    myStyle.backgroundColor = "red";
+                    myStyle.foreGroundColor = "black" ;
+                }
+
+            }
+        }
+    }
+
+    function swapValue(rowNum) {
+        var i = 0;
+        //console.log(rowNum);
+        for (i = 0; i < 6; i += 2) {
+            var temp = rows[rowNum].cells[i].innerHTML;
+            //console.log(typeof(temp));
+            rows[rowNum].cells[i].innerHTML = rows[rowNum].cells[i + 1].innerHTML;
+            rows[rowNum].cells[i + 1].innerHTML = temp;
+        }
+    }
