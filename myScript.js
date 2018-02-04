8598f8cf2aa3f9116b64887f1531261e16d14d51
lalit kumar <lalit.kumar@geminisolutions.in>
2018-02-04 23:35:12 +0530

    event bubbling added

--- myScript.js
+++ myScript.js
@@ -1,8 +1,28 @@
     var rows = document.getElementsByTagName("tr");
 
+function changeColorOdd(rowNum) {
+        var i = 0;
+        //console.log(rowNum);
+        // //console.log(rows[0].cells[0].innerHTML);
+        //console.log(rows[rowNum].cells.length);
+        for (i = 0; i < 6; i++) {
+            var myStyle = rows[rowNum].cells[i].style;
+            if (i % 2 !== 0) {
+                if (myStyle.backgroundColor == "purple") {
+                    myStyle.backgroundColor = "green";
+                    myStyle.foregroundColor = "yellow";
+                } else {
+                    myStyle.backgroundColor = "purple";
+                    myStyle.foreGroundColor = "black" ;
+                }
+
+            }
+        }
+    }
 
     function changeColor(rowNum) {
         var i = 0;
+       // rowNum.stopPropagation();
         //console.log(rowNum);
         // //console.log(rows[0].cells[0].innerHTML);
         //console.log(rows[rowNum].cells.length);
